2c6c7c8c068e48d478887d91e2e1041d0cca4fac
Bsdk0007 <princek8356@gmail.com>
2024-12-12 13:51:18 +0530

    Update config.py

--- config.py
+++ config.py
@@ -90,7 +90,7 @@ confirmer = {}
 
 
 START_IMG_URL = getenv(
-    "START_IMG_URL", "https://files.catbox.moe/bs5gfu"
+    "START_IMG_URL", "https://files.catbox.moe/pq7gnc"
 )
 PING_IMG_URL = getenv(
     "PING_IMG_URL", "https://envs.sh/nvp.jpg"
