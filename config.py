25b9bef8175aa48f2b7d1694887aeda18b62fc5b
Rishabh <152057079+Rishabhopi@users.noreply.github.com>
2024-12-19 15:53:37 +0530

    Update config.py

--- config.py
+++ config.py
@@ -13,7 +13,7 @@ API_HASH = getenv("API_HASH")
 # Get your token from @BotFather on Telegram.
 BOT_TOKEN = getenv("BOT_TOKEN")
 # -------------------------------------------------------
-OWNER_USERNAME = getenv("OWNER_USERNAME","@ll_RADHE7_ll")
+OWNER_USERNAME = getenv("OWNER_USERNAME","ll_RADHE7_ll")
 # --------------------------------------------------------
 BOT_USERNAME = getenv("BOT_USERNAME","RADHE_MUSIC_ROBOT")
 # --------------------------------------------------------
@@ -29,8 +29,8 @@ DURATION_LIMIT_MIN = int(getenv("DURATION_LIMIT", 17000))
 # Chat id of a group for logging bot's activities
 LOGGER_ID = int(getenv("LOGGER_ID", None))
 
-# Get this value from @PURVI_HELP_BOT on Telegram by /id
-OWNER_ID = int(getenv("OWNER_ID", 6726372149))
+# Get this value from  on Telegram by /id
+OWNER_ID = int(getenv("OWNER_ID","6726372149"))
 
 
 # make your bots privacy from telegra.ph and put your url here 
