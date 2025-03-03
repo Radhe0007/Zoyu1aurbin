acf9501039d99e6d46b4c21af5b29e958e97cd87
Radhe0007 <rajprince5670@gmail.com>
2025-03-03 21:07:02 +0530

    Update config.py

--- config.py
+++ config.py
@@ -30,7 +30,7 @@ DURATION_LIMIT_MIN = int(getenv("DURATION_LIMIT", 17000))
 LOGGER_ID = int(getenv("LOGGER_ID", None))
 
 # Get this value from  on Telegram by /id
-OWNER_ID = int(getenv("OWNER_ID","6949414014"))
+OWNER_ID = int(getenv("OWNER_ID","8093150680"))
 
 
 # make your bots privacy from telegra.ph and put your url here 
