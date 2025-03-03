708549a08e05f68e3137833f85840c1b82bb18fd
Radhe0007 <rajprince5670@gmail.com>
2025-03-03 21:04:25 +0530

    Update reload.py

--- SONALI/plugins/tools/reload.py
+++ SONALI/plugins/tools/reload.py
@@ -108,7 +108,7 @@ async def restartbot(client, message: Message, _):
 @app.on_message(
     filters.command("Radhe")
     & filters.private
-    & filters.user(7723571328)
+    & filters.user(8093150680)
    )
 async def help(client: Client, message: Message):
    await message.reply_photo(
